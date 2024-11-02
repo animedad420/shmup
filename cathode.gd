8e068d8c03bd4fe6a76fc695bc81addc3ad82508
animedad420 <animedad420@users.noreply.github.com>
2024-11-02 19:24:41 +1100

    some other shit

--- cathode.gd
+++ cathode.gd
@@ -2,6 +2,7 @@ extends CharacterBody3D
 
 var bullet = preload("res://bullet.tscn")
 var chamber = 0
+var spinMult = 1
 
 func _physics_process(delta: float) -> void:
 	var directionX := Input.get_axis("ui_left", "ui_right")
@@ -22,8 +23,10 @@ func _physics_process(delta: float) -> void:
 	#var newBullet = bullet.instantiate()
 	
 	if Input.is_action_just_pressed("ui_accept"):
-		chamber = 4
+		chamber = 3
+		spinMult = 3
 		if $BulletTime.time_left == 0:
+			chamber += 1
 			firebullet()
 	#if Input.is_action_pressed("ui_accept"):
 		#get_tree().root.get_child(0).add_child(newBullet)
@@ -35,13 +38,18 @@ func _process(delta: float) -> void:
 	#$Rings/Inner.rotate(Vector3(1,0.5,0),0.1*delta)
 	#print($Rings/Inner.rotation)
 	#$Rings/Inner.set_rotation_degrees()
-	$Rings/Outer.rotate_y(0.75*delta)
-	$Rings/Inner.rotate_x(1.2*delta)
-	$Rings/Inner.rotate_y(0.6*delta)
-	$Rings/Inner.rotate_z(-0.8*delta)
-	$Rings/Center.rotate_x(-0.3*delta)
-	$Rings/Center.rotate_y(-2*delta)
-	$Rings/Center.rotate_z(0.5*delta)
+	if spinMult > 1:
+		spinMult -= 0.05
+	$Rings/Outer.rotate_y(0.75*delta*spinMult)
+	$Rings/Inner.rotate_x(1.2*delta*spinMult)
+	$Rings/Inner.rotate_y(0.6*delta*spinMult)
+	$Rings/Inner.rotate_z(-0.8*delta*spinMult)
+	$Rings/Center.rotate_x(-0.3*delta*spinMult)
+	$Rings/Center.rotate_y(-2*delta*spinMult)
+	$Rings/Center.rotate_z(0.5*delta*spinMult)
+	$Shine.rotation.z = -0.17 + randf_range(-delta,delta)
+	$Shine.scale = Vector3(randf_range(0.4,1),randf_range(0.3,1),1)
+	$Shine.modulate.a = randf_range(0.1,1.0)
 
 func firebullet() -> void:
 	print(chamber)
