904bbe038bc792e725030acf83c26ea3f46fd63a
animedad420 <animedad420@users.noreply.github.com>
2024-11-12 10:58:12 +1100

    enemy bullet

--- cube.gd
+++ cube.gd
@@ -1,11 +1,18 @@
 extends RigidBody3D
 
 var inBeam: Node
+var health: int = 20
+signal health_depleted
+var bullet = preload("res://enemybullet.tscn")
+var direction: Vector3
+var chamber = 0
 
 func _physics_process(delta: float) -> void:
 	$Cube.rotate_x(1.2*delta)
 	$Cube.rotate_y(0.6*delta)
 	$Cube.rotate_z(-0.8*delta)
+	
+	
 
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
@@ -20,7 +27,10 @@ func _on_area_3d_area_exited(area: Area3D) -> void:
 
 
 func take_damage(attacker: Node) -> void:
-	print(attacker.damage)
+	#print(attacker.damage)
+	health -= attacker.damage
+	if health <= 0:
+		call_deferred("queue_free")
 	$Sprite3D.material_override.set_shader_parameter("intensity",1)
 	$Cube.material_override.set_shading_mode(0)
 	$FlashTime.start()
@@ -37,3 +47,29 @@ func _on_flash_time_timeout() -> void:
 func _on_beam_check_timeout() -> void:
 	if inBeam != null:
 		take_damage(inBeam)
+
+
+func _on_health_depleted() -> void:
+	if health <= 0:
+		call_deferred("queue_free")
+
+
+func _on_bullet_wait_timeout() -> void:
+	chamber = 3
+	firebullet()
+
+
+func _on_bullet_load_timeout() -> void:
+	firebullet()
+
+
+func firebullet():
+	if chamber > 0:
+		var newBullet = bullet.instantiate()
+		get_tree().root.get_child(0).add_child(newBullet)
+		newBullet.global_position = $".".global_position
+		direction = (get_parent().get_node("Cathode").global_position - $".".global_position).normalized()
+		
+		newBullet.set_axis_velocity(direction*1.8)
+		chamber -= 1
+		$BulletLoad.start()
